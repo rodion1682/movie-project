62ad0eb7b1a626b3eb2d1ed65093d156b600ec56
rodion1682 <rodiontropkin17@gmail.com>
2023-09-30 11:30:00 +0300

    fix paths, and eslint errors

--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -2,23 +2,31 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
+import { useTranslation } from 'react-i18next';
 import cls from './NavBar.module.scss';
 
 interface NavBarProps {
 	className?: string;
 }
 
-export const NavBar = ({ className }: NavBarProps) => (
-	<div className={classNames(cls.NavBar, {}, [className])}>
-		<ThemeSwitcher />
-		<LangSwitcher className={cls.lang} />
-		<div className={cls.links}>
-			<AppLink to="/" theme={AppLinkTheme.PRIMARY} className={cls.mainLink}>
-				Main page
-			</AppLink>
-			<AppLink to="/about" theme={AppLinkTheme.PRIMARY}>
-				About page
-			</AppLink>
+export const NavBar = ({ className }: NavBarProps) => {
+	const { t } = useTranslation();
+	return (
+		<div className={classNames(cls.NavBar, {}, [className])}>
+			<ThemeSwitcher />
+			<LangSwitcher className={cls.lang} />
+			<div className={cls.links}>
+				<AppLink
+					to="/"
+					theme={AppLinkTheme.PRIMARY}
+					className={cls.mainLink}
+				>
+					{t('Main page')}
+				</AppLink>
+				<AppLink to="/about" theme={AppLinkTheme.PRIMARY}>
+					{t('About us')}
+				</AppLink>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
